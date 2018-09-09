cd721fe99ce8c3183cdeaa74d665870e95d4f3cd
vinnix <vinnix.bsd@gmail.com>
2018-09-09 09:36:48 +0100

    Fixing PEP8 issues. Thanks to pyCharm

--- bin/getNparse.py
+++ bin/getNparse.py
@@ -1,60 +1,63 @@
 #!/bin/env python
 
-
 #
 # Parsing URLs to gather PostgreSQL info from "awesome" lists
 #
 
 import re
+import urllib.request
 
 lines_array = []
 lines_to_parse = []
 url_list_started = False
 parsing_list = []
 try:
-    with open("../README.md","rt") as readme_file:
-      for line in readme_file:
-         lines_array.append(line.rstrip('\n'))
+    with open("../README.md", "rt") as readme_file:
+        for line in readme_file:
+            lines_array.append(line.rstrip('\n'))
 
-    url_start_exp = re.compile(r"BEGIN.*URL_LIST_TO_PARSE",re.IGNORECASE)
-    url_end_exp = re.compile(r"END.*URL_LIST_TO_PARSE",re.IGNORECASE)
+    url_start_exp = re.compile(r"BEGIN.*URL_LIST_TO_PARSE", re.IGNORECASE)
+    url_end_exp = re.compile(r"END.*URL_LIST_TO_PARSE", re.IGNORECASE)
 
-    for linenum,element in enumerate(lines_array):
+    for linenum, element in enumerate(lines_array):
+        # Only check if the list mark/comment that points to the end of the URL list
+        # if we are already in appending mode. Otherwise there is no point to very it.
+        if url_list_started and url_end_exp.search(element):
+            url_list_started = False
 
-      # Only check if the list mark/comment that points to the end of the URL list
-      # if we are already in appending mode. Otherwise there is no point to very it.
-      if url_list_started and url_end_exp.search(element):
-          url_list_started = False
+        if url_list_started:
+            lines_to_parse.append(element)
 
-      if url_list_started:
-         lines_to_parse.append(element)
+        # The first boolean verification of the expression below is to avoid unnecessary
+        # RE search.
+        if url_list_started or url_start_exp.search(element):
+            url_list_started = True
 
-      # The first boolean verification of the expression below is to avoid unnecessary
-      # RE search.
-      if url_list_started or url_start_exp.search(element):
-          url_list_started = True
+        # XXX Another possibility is: once knowing the "linenum" of the BEGIN and the END
+        # just do for loop and append the array, but it seems to be extra work.
+        # Need to confirm if this makes sense. That's why I tried to avoid unnecessary regular
+        # expressions check. Ideally this script should be able
+        # to parse large files in a non-expensive way.
 
-      ## XXX Another possibility is: once knowing the "linenum" of the BEGIN and the END
-      ## just do for loop and append the array, but it seems to be extra work.
-      ## Need to confirm if this makes sense. That's why I tried to avoid unnecessary regular
-      ## expressions check. Ideally this script should be able to parse large files in a non-expensive way.
+    for urlnum, to_parse in enumerate(lines_to_parse):
+        # XXX: I will come back to this regex later
+        # https://docs.python.org/3.6/library/re.html
+        url_to_parse = re.search("RAW\((?P<url>https?://[^\s]+)\)", to_parse)
+        # print(to_parse, urlnum)
+        # print(url_to_parse.group("url"))
+        # print("\n\n")
+        parsing_list.append(url_to_parse.group("url"))
+        # I do still have more to learn regarding regex in python
 
-
-    for urlnum,to_parse in enumerate(lines_to_parse):
-      ### XXX: I will come back to this regex later
-      ### https://docs.python.org/3.6/library/re.html
-      url_to_parse = re.search("RAW\((?P<url>https?://[^\s]+)\)", to_parse)
-      #print(to_parse,urlnum)
-      #print(url_to_parse.group("url"))
-      #print("\n\n")
-      parsing_list.append(url_to_parse.group("url"))
-      ### I do still have more to learn regarding regex in python
-
-    for pos,url in enumerate(parsing_list):
+    for pos, url in enumerate(parsing_list):
         print(url)
 
-
 except FileNotFoundError:
-    print("vinnix's README file not found!")
+    print("vinnix's README.md file not found!")
 
 
+def url_parse(address):
+    req = urllib.request.Request(address)
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read()
+    return the_page
